acfa1cea9ed7917d9567cd30983bb9005d5a6a4c
LukeJansen <lpjansen@live.co.uk>
2019-03-11 16:21:28 +0000

    Alpha 04
    - Fixed UI Bugs
    - Added first scenario
    - Added character

---
+++ ParentPal/Assets/Scripts/ResponsiveUI.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.UI;
+
+public class ResponsiveUI : MonoBehaviour
+{
+    private int screenWidth, screenHeight;
+    private int minButtonHeight, maxButtonHeight;
+    private float titleShare, textShare, buttonShare, imageShare;
+
+    public RectTransform scenarioText, scenarioTitle, scenarioImage;
+    public List<GameObject> buttons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GrabScreenSize();
+
+        minButtonHeight = 50;
+        maxButtonHeight = 200;
+
+        // The percentage shares each element should take up of the screen.
+        // These values should add up to 1!
+        titleShare = 0.1f;
+        textShare = 0.5f;
+        buttonShare = 0.4f;
+        //Vertical screen percantage for the image.
+        imageShare = 0.4f;
+
+        setup();
+    }
+
+    // Function to setup the main parts of the UI
+    private void setup()
+    {
+        RectTransform mainPanel = GetComponent<RectTransform>();
+
+        // Resize the background to match the phone's resolution.
+        mainPanel.sizeDelta = new Vector2(screenWidth, screenHeight);
+
+        // Resize the scenario's text box to the correct proportions and position.
+        scenarioText.sizeDelta = new Vector2(screenWidth * (1 - imageShare), screenHeight * textShare);
+        scenarioText.anchoredPosition = new Vector2(-(screenWidth * ((1 - imageShare) * 0.5f)), -(screenHeight * (titleShare)));
+
+        // Resize the scenario's image to the correct proportions and position.
+        scenarioImage.sizeDelta = new Vector2(screenWidth * imageShare, screenHeight * textShare);
+        scenarioImage.anchoredPosition = new Vector2(screenWidth * (imageShare * 0.5f), -(screenHeight * titleShare) - (scenarioImage.sizeDelta.y * 0.5f));
+
+        // Move the scenario's title to the correct position.
+        scenarioTitle.sizeDelta = new Vector2(screenWidth, screenHeight * titleShare);
+        scenarioTitle.anchoredPosition = new Vector2(0, -(screenHeight * (titleShare * 0.5f)));
+    }
+
+    // Function to grab the size of the screen.
+    private void GrabScreenSize()
+    {
+        screenWidth = Screen.width;
+        screenHeight = Screen.height;
+    }
+
+    // Function to setup the buttons for the current scenario.
+    public void SetupButtons(List<GameObject> scenarioButtons)
+    {
+        GrabScreenSize();
+        buttons = scenarioButtons;
+
+        // Loop through all of the buttons:
+        for (int i = 0; i < buttons.Count; i++)
+        {
+            RectTransform buttonTransform = buttons[i].GetComponent<RectTransform>();
+
+            // Resize the button's to correct proportions.
+            float buttonHeight = Mathf.Clamp((screenHeight * buttonShare) / (buttons.Count + 1), minButtonHeight, maxButtonHeight);
+            float buttonWidthScale = 0.75f;
+            buttonTransform.sizeDelta = new Vector2(screenWidth * buttonWidthScale, buttonHeight);
+
+            // Move the buttons to the correct positions.
+            float ySpacing = buttonHeight / (buttons.Count + 1);
+            float y = ((buttonHeight * ((buttons.Count + 1) - i)) - (ySpacing * (i + 1))) - (buttonHeight * 0.5f);
+            buttonTransform.anchoredPosition = new Vector3(0f, y, 0f);
+        }
+    }
+}
--- ParentPal/Assets/Scripts/ScenarioController.cs
+++ ParentPal/Assets/Scripts/ScenarioController.cs
@@ -9,7 +9,7 @@ public class ScenarioController : MonoBehaviour
     public GameObject buttonPrefab;
     public Text titleText, textText;
 
-    private Scenario scenario1, scenario2, scenario3, scenario4;
+    private Scenario scenario1, scenario2, scenario3, scenario4, scenario5;
     private Option option1, option2, option3, option4, option5;
     private List<GameObject> buttonList;
 
@@ -19,23 +19,30 @@ public class ScenarioController : MonoBehaviour
     {
         buttonList = new List<GameObject>();
 
-        scenario1 = new Scenario("Scenario 1", "Child is born");
+        scenario1 = new Scenario("Scenario 1", "Teen sat playing games console in messy bedroom. Carer knocks on the door and walks in..." + 
+            "\n\n1) Carer flies off the handle... We’ve got to go out soon – get off that blasted computer! and look at the state of this room!When is the last time you cleaned it ? " +
+            "\n\n2) Carer remains calm: Insert teen name, we’re leaving in half an hour.Can you finish playing your game, get changed and be ready in the kitchen by 7 o’clock.I will give you a reminder in 15 minutes.If you are still on the console, I will turn it off. " +
+            "\n\n3) Carer shouts upstairs to teen, “We need to go out soon.” Teen shouts back, “ok.” Without diverting their attention from the game." +
+            "\n\n4) Carer quietly enters teen’s room and requests they come off the computer game to go out later.Teen isn’t really listening");
+        
+        option1 = new Option("Carer flies off the handle", scenario2);
+        option2 = new Option("Carer remains calm", scenario3);
+        option3 = new Option("Carer shouts upstairs", scenario4);
+        option4 = new Option("Carer quietly enters room", scenario5);
+
+        List<Option> tempList = new List<Option>();
+        tempList.Add(option1);
+        tempList.Add(option2);
+        tempList.Add(option3);
+        tempList.Add(option4);
+        scenario1.Options = tempList;
+
         scenario2 = new Scenario("Scenario 2", "Child says first word");
-        scenario3 = new Scenario("Scenario 3", "Child falls over and is hurt");
-        scenario4 = new Scenario("Scenario 4", "Child is now a teen");
 
         option1 = new Option("Go to Scenario 1", scenario1);
         option2 = new Option("Go to Scenario 2", scenario2);
         option3 = new Option("Go to Scenario 3", scenario3);
         option4 = new Option("Go to Scenario 4", scenario4);
-        option5 = new Option("End Screen");
-
-
-        List<Option> tempList = new List<Option>();
-        tempList.Add(option2);
-        tempList.Add(option3);
-        tempList.Add(option4);
-        scenario1.Options = tempList;
 
         tempList = new List<Option>();
         tempList.Add(option1);
@@ -43,11 +50,25 @@ public class ScenarioController : MonoBehaviour
         tempList.Add(option4);
         scenario2.Options = tempList;
 
+        scenario3 = new Scenario("Scenario 3", "Child falls over and is hurt");
+
+        option1 = new Option("Go to Scenario 1", scenario1);
+        option2 = new Option("Go to Scenario 2", scenario2);
+        option3 = new Option("Go to Scenario 3", scenario3);
+        option4 = new Option("Go to Scenario 4", scenario4);
+
         tempList = new List<Option>();
         tempList.Add(option1);
         tempList.Add(option2);
         tempList.Add(option4);
         scenario3.Options = tempList;
+
+        scenario4 = new Scenario("Scenario 4", "Child is now a teen");
+
+        option1 = new Option("Go to Scenario 1", scenario1);
+        option2 = new Option("Go to Scenario 2", scenario2);
+        option3 = new Option("Go to Scenario 3", scenario3);
+        option4 = new Option("Go to Scenario 4", scenario4);
 
         tempList = new List<Option>();
         tempList.Add(option5);
