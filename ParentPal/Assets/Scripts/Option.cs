624ece5ab3c6fe9a42fc779443d9bc43ac3b4e96
TomaszWalat <33025239+TomaszWalat@users.noreply.github.com>
2019-05-06 19:36:33 +0100

    Revert "Revert "Added description to Options""
    This reverts commit 73cbcdddb805d75bf7508bfb6b4dbe56f0fb83cb.

--- ParentPal/Assets/Scripts/Option.cs
+++ ParentPal/Assets/Scripts/Option.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Option 
 {
     public string Text;
+    public string Description;
     public int Link;
 
     public static Option CreateFromJson(string jsonString)
