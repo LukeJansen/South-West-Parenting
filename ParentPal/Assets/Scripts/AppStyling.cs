2038e7e21cffc603fd90ffc0bfddc37c7ea857f0
TomaszWalat <33025239+TomaszWalat@users.noreply.github.com>
2019-05-08 21:26:19 +0100

    Alpha 16
    - Added 2 recourse packs
    - Started reskinning the UI

--- ParentPal/Assets/Scripts/AppStyling.cs
+++ ParentPal/Assets/Scripts/AppStyling.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AppStyling : MonoBehaviour
 {
@@ -9,22 +10,81 @@ public class AppStyling : MonoBehaviour
     private Color cardColour;
     private Color backgroundColour;
 
+    private Sprite backgroundSprite;
+    private Sprite buttonSprite;
+    private Sprite cardSprite;
+    private Sprite crossButtonSprite;
+    private Sprite homeButtonSprite;
+
+    private Image[] mainCameraChildren;
+
     [SerializeField]
-    private Canvas canvas;
+    private GameObject panel;
 
     void Awake()
     {
+        //backgroundSprite = (Sprite)Resources.Load(@"Assets/Resources/blue_button11.png");
+        //buttonSprite = (Sprite)Resources.Load("Assets/ThirdPartyAssets/uipack_fixed/PNG/blue_button11.png");
+        //cardSprite = (Sprite)Resources.Load("Assets/ThirdPartyAssets/uipack_fixed/PNG/blue_button11.png");
+        //crossButtonSprite = (Sprite)Resources.Load("Assets/ThirdPartyAssets/uipack_fixed/PNG/blue_button11.png");
+        //homeButtonSprite = (Sprite)Resources.Load("Assets/ThirdPartyAssets/uipack_fixed/PNG/blue_button11.png");
+
         buttonColour = new Color(0.345f, 0.8f, 0.976f);
         cardColour = new Color(0.741f, 0.913f, 0.992f, 1f);
         backgroundColour = new Color(1f, 1f, 1f, 1f);
 
+        Debug.Log(backgroundSprite);
+
         //SetColours(canvas);
     }
 
-    //private void SetColours(Canvas canvas)
+    //void Start()
     //{
+    //    Invoke("UpdateAppStyling", 0.1f);
+    //}
+
+    //public void UpdateAppStyling()
+    //{
+    //    //mainCameraChildren = new List<RectTransform>();
+
+    //    Debug.Log("update app styling called");
+
+    //    mainCameraChildren = GetComponentsInChildren<Image>();
+
+    //    foreach(Image child in mainCameraChildren)
+    //    {
+    //        //GameObject child = obj.GetComponent<GameObject>();
+
+    //        if(child.tag == "background")
+    //        {
+    //            panel.GetComponent<Image>().overrideSprite = backgroundSprite;
+    //        }
+    //        else if(child.tag == "card")
+    //        {
+    //            child.GetComponent<Image>().sprite = buttonSprite;
+    //        }
+    //        else if(child.tag == "button")
+    //        {
+    //            child.GetComponent<Image>().sprite = cardSprite;
+    //        }
+    //        else if (child.tag == "homeButton")
+    //        {
+    //            child.GetComponent<Image>().sprite = crossButtonSprite;
+    //        }
+    //        else if (child.tag == "crossButton")
+    //        {
+    //            child.GetComponent<Image>().sprite = homeButtonSprite;
+    //        }
+
+            
+    //    }
 
     //}
+
+    private void SetColours(Canvas canvas)
+    {
+
+    }
 
     public Color ButtonColour {
         get
